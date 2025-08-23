0d1f37d14f8596d974ae7cb0c4bef8636eecc268
Volha Yurchanka <volha@yurchanka.ru>
2025-08-23 20:03:25 +0300

    feat: add solution of towelSort function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (arguments.length === 0) {
+    return [];
+  }
+  const copyMatrix = matrix.map((subArr) => [...subArr]);
+  copyMatrix.forEach((subArr, index) => {
+    if (index % 2 !== 0) {
+      copyMatrix[index] = subArr.reverse();
+    }
+  });
+  return copyMatrix.flat();
 };
